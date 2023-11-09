a5ad39296d945ac560601c91ffe9dcb462e3a50f
elij <elij@wundrd.net>
2023-11-08 19:00:12 -0800

    update

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/dropwhile/ldevprox
+
+go 1.21.4
+
+require (
+	github.com/alecthomas/kong v0.8.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/rs/zerolog v1.31.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ proxy.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+
+	"github.com/alecthomas/kong"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type verboseFlag bool
+
+func (v verboseFlag) BeforeApply() error {
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	log.Debug().Msg("debug logging enabled")
+	return nil
+}
+
+type CLI struct {
+	Listen   string           `name:"listen" short:"l" default:"127.0.0.1:8080" help:"listen address:port"`
+	TLSCert  string           `name:"tls-cert" default:"server.cert" help:"tls cert file path"`
+	TLSKey   string           `name:"tls-key" default:"server.key" help:"tls key file path"`
+	Upstream string           `name:"upstream" default:"http://127.0.0.1:8000" help:"upstream url"`
+	Verbose  verboseFlag      `name:"verbose" short:"v" help:"enable verbose logging"`
+	Version  kong.VersionFlag `name:"version" short:"V" help:"Print version information and quit"`
+}
+
+func (cmd *CLI) Run() error {
+	localProxyUrl, _ := url.Parse(cmd.Upstream)
+	localProxy := &httputil.ReverseProxy{
+		Rewrite: func(r *httputil.ProxyRequest) {
+			r.SetURL(localProxyUrl)
+			r.SetXForwarded()
+		},
+	}
+	//localProxy := httputil.NewSingleHostReverseProxy(localProxyUrl)
+	http.Handle("/", localProxy)
+	log.Printf("Serving on %s\n", cmd.Listen)
+	if err := http.ListenAndServeTLS(cmd.Listen, cmd.TLSCert, cmd.TLSKey, nil); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Fatal().Err(err).Msg("Server error")
+		return err
+	}
+	return nil
+}
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{
+		Out:          os.Stderr,
+		PartsExclude: []string{zerolog.TimestampFieldName},
+	})
+
+	cli := CLI{}
+	ctx := kong.Parse(&cli,
+		kong.Name("ldevprox"),
+		kong.Description("A local ssl proxy for development"),
+		kong.UsageOnError(),
+		kong.Vars{
+			"version": "0.0.1",
+		},
+	)
+	err := ctx.Run(&cli)
+	ctx.FatalIfErrorf(err)
+}
